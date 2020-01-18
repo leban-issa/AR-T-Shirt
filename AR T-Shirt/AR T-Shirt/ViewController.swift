7e66ff0e540f9563d11260e883f5752ac47847d5
leban-issa <60042716+leban-issa@users.noreply.github.com>
2020-01-18 12:31:25 -0500

    Add files via upload

---
+++ AR T-Shirt/AR T-Shirt/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  AR T-Shirt
+//
+//  Created by Leban Issa on 2019-06-18.
+//  Copyright © 2019 Leban Issa. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        // first see if there is a folder called "ARImages" Resource Group in our Assets Folder
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main) {
+            
+            // if there is, set the images to track
+            configuration.trackingImages = trackedImages
+            // at any point in time, only 1 image will be tracked
+            configuration.maximumNumberOfTrackedImages = 1
+        }
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // if the anchor is not of type ARImageAnchor (which means image is not detected), just return
+        guard let imageAnchor = anchor as? ARImageAnchor, let fileUrlString = Bundle.main.path(forResource: "kawhigamewinner", ofType: "mp4") else {return}
+        //find our video file
+        
+        let videoItem = AVPlayerItem(url: URL(fileURLWithPath: fileUrlString))
+        
+        let player = AVPlayer(playerItem: videoItem)
+        //initialize video node with avplayer
+        let videoNode = SKVideoNode(avPlayer: player)
+        
+        player.play()
+        // add observer when our player.currentItem finishes player, then start playing from the beginning
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: nil) { (notification) in
+            player.seek(to: CMTime.zero)
+            player.play()
+            print("Looping Video")
+        }
+        
+        
+        //sets the size of the video
+        let videoScene = SKScene(size: CGSize(width: 480, height: 360))
+        // center our video to the size of our video scene
+        videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+        // invert our video so it does not look upside down
+        videoNode.yScale = -1.0
+        // add the video to our scene
+        videoScene.addChild(videoNode)
+        // create a plan that has the same real world height and width as our detected image
+        let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+        // set the first materials content to be our video scene
+        plane.firstMaterial?.diffuse.contents = videoScene
+        // create a node out of the plane
+        let planeNode = SCNNode(geometry: plane)
+        // since the created node will be vertical, rotate it along the x axis to have it be horizontal or parallel to our detected image
+        planeNode.eulerAngles.x = -Float.pi / 2
+        // finally add the plane node (which contains the video node) to the added node
+        node.addChildNode(planeNode)
+    }
+}
---
+++ AR T-Shirt/AR T-ShirtTests/AR_T_ShirtTests.swift
@@ -0,0 +1,34 @@
+//
+//  AR_T_ShirtTests.swift
+//  AR T-ShirtTests
+//
+//  Created by Leban Issa on 2019-06-18.
+//  Copyright © 2019 Leban Issa. All rights reserved.
+//
+
+import XCTest
+@testable import AR_T_Shirt
+
+class AR_T_ShirtTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
